8c41e13f591d518ce9e93d50c3a50cdd59008dd0
Dmitrii Kushnerev <65852565+w0lrid@users.noreply.github.com>
2022-12-03 20:46:07 +0600

    Feature: add footer (#26)
    * feat: add footer desktop
    
    * feat: set footer mobile; common styles improvements
    
    * fix: lint fixes
    
    * fix: rename age block
    
    * fix: rename age block
    
    * feat: add new color variable
    
    * feat: update color using existing variable
    
    * feat: add footer links
    
    * feat: add links for menu
    
    * feat: common refactoring; implement submenu
    
    * feat: define new footer distances
    
    * refactor: update footer logo, socials and common improvements
    
    * lint fixes
    
    * lint fixes
    
    * refactor: bem improvements
    
    * feat: add resize function; refactor
    
    * feat: add donate footer
    
    * refactor: update common footer modifier
    
    * refactor: update main footer for mobile; refactor

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 import Carousel from './carousel.js';
+import checkScreenSize from './utils/checkScreenSize.js';
+import toggleSubmenu from './toggleSubmenu.js';
 
 const heroEl = document.querySelector('.hero-banner');
 // eslint-disable-next-line no-unused-vars
@@ -9,3 +11,10 @@ const friendsEl = document.querySelector('.our-friends > .carousel');
 // eslint-disable-next-line no-unused-vars
 const friendsCarousel = new Carousel(friendsEl);
 // friendsCarousel.start();
+
+const footerSubmenuButton = document.querySelector('.footer__menu-button');
+const programSubmenu = document.querySelector('.submenu_type_program');
+
+window.addEventListener('resize', () => checkScreenSize(), { passive: true });
+
+toggleSubmenu(footerSubmenuButton, programSubmenu);
