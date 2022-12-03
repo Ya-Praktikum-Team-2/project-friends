a028255da1dfc4c08c40b803594158231b77ce97
Dmitrii Kushnerev <w0lridem@gmail.com>
2022-12-03 18:23:50 +0600

    feat: common refactoring; implement submenu

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,14 @@ const friendsEl = document.querySelector('.our-friends > .carousel');
 // eslint-disable-next-line no-unused-vars
 const friendsCarousel = new Carousel(friendsEl);
 // friendsCarousel.start();
+
+const footerSubmenuButton = document.querySelector('.footer__menu-button');
+const programSubmenu = document.querySelector('.submenu_type_program');
+
+function toggleSubmenu(element, submenu) {
+  element.addEventListener('click', () => {
+    submenu.classList.toggle('submenu_active');
+  });
+}
+
+toggleSubmenu(footerSubmenuButton, programSubmenu);
