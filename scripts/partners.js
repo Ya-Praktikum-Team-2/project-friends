d97508167da2ab2a78910e2dc63fc21db1fceef5
Maksim Perevalov <skai-net1989@mail.ru>
2022-12-08 16:20:04 +0300

    Mini fix

--- scripts/partners.js
+++ scripts/partners.js
@@ -3,10 +3,10 @@ const partnersSliderLine = document.querySelector('.partners__slider-line');
 const buttonNext = document.querySelector('.partners__button-right');
 const buttonPrev = document.querySelector('.partners__button-left');
 
-buttonNext.addEventListener('click', function(){
+buttonNext.addEventListener('click', function() {
   offset += 362;
   if (offset >= 943) {
-    offset = 943
+    offset = 943;
     buttonNext.classList.add('partners_hidden');
   }
   partnersSliderLine.style.left = -offset + 'px';
@@ -15,7 +15,7 @@ buttonNext.addEventListener('click', function(){
   }
 });
 
-buttonPrev.addEventListener('click', function(){
+buttonPrev.addEventListener('click', function() {
   offset -= 362;
   if (offset <= 0) {
     offset = 0;
