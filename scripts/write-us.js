e65e416a17544edb12cd71b3947e94f66442a4c7
Maksim Perevalov <skai-net1989@mail.ru>
2022-12-10 00:23:35 +0300

    fix write-us.js

--- scripts/write-us.js
+++ scripts/write-us.js
@@ -1,6 +1,5 @@
 const popupWriteUs = document.querySelector('.popup-write-us');
 const popupWriteUForm = document.querySelector('.write-us__form');
-const popupWriteUsSubmit = document.querySelector('.write-us__button-submit');
 const popupWriteUsClose = document.querySelector('.popup-write-us__button-close');
 
 const openPopupWriteUs = function (popupName) {
@@ -12,9 +11,6 @@ popupWriteUForm.addEventListener('submit', function (evt) {
   openPopupWriteUs(popupWriteUs);
 });
 
-
 popupWriteUsClose.addEventListener('click', function () {
-  popupWriteUs.classList.remove('popup-write-us_opened')
+  popupWriteUs.classList.remove('popup-write-us_opened');
 });
-
-
