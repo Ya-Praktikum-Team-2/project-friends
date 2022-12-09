21aabf96cc30dea819af63a1edae8e91d77fb388
Vladislav <112831869+chvllad@users.noreply.github.com>
2022-12-09 15:23:58 +0300

    Header. Round 1

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ import Carousel from './utils/carousel.js';
 import ScreenSizeTracker from './utils/ScreenSizeTracker.js';
 import toggleSubmenu from './toggleSubmenu.js';
 import Blog from './blog.js';
+import Header from './header.js';
 
 const heroEl = document.querySelector('.hero-banner');
 // eslint-disable-next-line no-unused-vars
@@ -17,6 +18,9 @@ const friendsCarousel = new Carousel(friendsEl);
 
 const blog = new Blog(document.querySelector('.blog'));
 
+// eslint-disable-next-line no-unused-vars
+const header = new Header(document.querySelector('.header'));
+
 ScreenSizeTracker.addListener(768, (isBigger) => {
   if (isBigger) {
     blog.setNormal();
@@ -24,6 +28,10 @@ ScreenSizeTracker.addListener(768, (isBigger) => {
     // TODO: enable carousel
     blog.setCarousel(false);
   }
+});
+
+ScreenSizeTracker.addListener(576, (isBigger) => {
+  header.onViewCnahge(!isBigger);
 });
 
 const footerSubmenuButton = document.querySelector('.footer__menu-button');
